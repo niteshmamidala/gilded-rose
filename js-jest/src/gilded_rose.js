87594dbaffdd3a2397a27ccb1c9270e0813db023
Nitesh Mamidala <nitesh.mamidala7@gmail.com>
2022-08-11 01:20:06 +0530

    Refactored extracting common functionality

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -6,59 +6,70 @@ class Item {
     }
 }
 
-const itemName = {
+const ITEM_NAME = {
     AGED_BRIE: 'Aged Brie',
     BACK_STAGE_PASSES: 'Backstage passes to a TAFKAL80ETC concert',
     SULFURAS: 'Sulfuras, Hand of Ragnaros'
 };
 
-const handleAgedBrieItem = (item) => {
+const increaseQuality = item => {
     if (item.quality < 50) {
         item.quality = item.quality + 1;
     }
+};
 
-    item.sellIn = item.sellIn - 1;
-
-    if (item.sellIn < 0 && item.quality < 50) {
-        item.quality = item.quality + 1;
-    }
-}
-
-const handleBackStagePassesItem = (item) => {
-    if (item.quality < 50) {
-        item.quality = item.quality + 1;
-
-        if (item.sellIn < 11 && item.quality < 50) {
-            item.quality = item.quality + 1;
-        }
-
-        if (item.sellIn < 6 && item.quality < 50) {
-            item.quality = item.quality + 1;
-        }
-    }
-
-    item.sellIn = item.sellIn - 1;
-
-    if (item.sellIn < 0) {
-        item.quality = item.quality - item.quality;
-    }
-}
-
-const handleSulfurasItem = (item) => {
-    // do nothing as of now
-}
-
-const handleNormalItem = (item) => {
+const decreaseQuality = item => {
     if (item.quality > 0) {
         item.quality = item.quality - 1;
     }
+};
 
+const updateSellIn = item => {
     item.sellIn = item.sellIn - 1;
+};
 
-    if (item.sellIn < 0 && item.quality > 0) {
-        item.quality = item.quality - 1;
+const handleAgedBrieItem = item => {
+    increaseQuality(item);
+    updateSellIn(item);
+
+    // “Aged Brie” actually increases in Quality the older it gets
+    if (item.sellIn < 0) {
+        increaseQuality(item);
     }
-}
+};
+
+const handleBackStagePassesItem = item => {
+    increaseQuality(item);
+
+    if (item.sellIn < 11) {
+        increaseQuality(item);
+    }
+
+    if (item.sellIn < 6) {
+        increaseQuality(item);
+    }
+
+    updateSellIn(item);
+
+    // Quality drops to 0 after the concert
+    if (item.sellIn < 0) {
+        item.quality = 0;
+    }
+};
+
+const handleSulfurasItem = item => {
+    // do nothing as of now
+};
+
+const handleNormalItem = item => {
+    decreaseQuality(item);
+    updateSellIn(item);
+
+    // Once the sell by date has passed, Quality degrades twice as fast
+    if (item.sellIn < 0) {
+        decreaseQuality(item);
+    }
+};
 
 class Shop {
     constructor(items=[]){
@@ -68,13 +79,13 @@ class Shop {
     updateQuality() {
         for (const item of this.items) {
             switch (item.name) {
-                case itemName.AGED_BRIE:
+                case ITEM_NAME.AGED_BRIE:
                     handleAgedBrieItem(item);
                     continue;
-                case itemName.BACK_STAGE_PASSES:
+                case ITEM_NAME.BACK_STAGE_PASSES:
                     handleBackStagePassesItem(item);
                     continue;
-                case itemName.SULFURAS:
+                case ITEM_NAME.SULFURAS:
                     handleSulfurasItem(item);
                     continue;
                 default:
